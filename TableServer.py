b0c68930b05163d9acfc76bf2a35a853d072da16
ilya0 <iosovets@gmail.com>
2018-10-03 13:56:26 -0700

    added routes for nfc and postingjson

--- TableServer.py
+++ TableServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,json,request
 from jinja2 import Template
 import csv
 
@@ -21,6 +21,27 @@ def main():
 
     # Generate HTML
     return render_template('index.html', table=table)
+
+@app.route('/nfc/<nfcid>')
+def api_article(nfcid):
+    return 'You are reading ' + nfcid
+    
+
+@app.route('/postjson', methods = ['POST'])
+def postJsonHandler():
+    print (request.is_json)
+    content = request.get_json()
+    print (content)
+    print(content["device"])
+    
+    return 'JSON posted'
+
+#test json to be sent
+#     { "device":"TemperatureSensor", 
+#  "value":"20", 
+#  "timestamp":"25/01/2017 10:10:05" 
+# }
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
